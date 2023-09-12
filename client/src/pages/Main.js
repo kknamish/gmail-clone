93cb2d21bcdacbf74fedc210d95b68e1c1018b0b
Kuchar Kal Namish <Namish@Kuchars-MacBook-Air-2.local>
2023-09-13 00:58:52 +0530

    Removing clutter and structuring dir

---
+++ client/src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+
+const Main = () => {
+  return (
+    <>
+        <Header />
+        <Sidebar />
+        <div>Display Mail</div>
+    </>
+  )
+}
+
+export default Main
